fe83a527b5801306b37b9f69a17080d49eae7959
YeonV <trusted.yeon@gmail.com>
2025-10-09 01:23:20 +0200

    Release 0.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/stream-locker/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/stream-locker/' : '/',
 })
